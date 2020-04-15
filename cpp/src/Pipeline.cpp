fea4fae1bbca2b78c40a8f1c96176234a57eda7a
Daniel Heater <daniel@heatertech.com>
2020-04-15 18:26:01 -0500

    Use early exits instead of else statements

--- cpp/src/Pipeline.cpp
+++ cpp/src/Pipeline.cpp
@@ -2,41 +2,41 @@
 
 void Pipeline::run(Project project)
 {
-    if(project.hasTests()) {
-        if("success" == project.runTests()) {
-            log.info("Tests passed");
-            deploy("success" == project.deploy());
-        }
-        else {
-            log.error("Tests failed");
-            send_email_if_enabled("Tests failed");
-        }
-    }
-    else {
+    if(!project.hasTests()) {
         log.info("No tests");
         deploy("success" == project.deploy());
+        return;
     }
+
+    if(!("success" == project.runTests())) {
+        log.error("Tests failed");
+        send_email_if_enabled("Tests failed");
+        return;
+    }
+
+    log.info("Tests passed");
+    deploy("success" == project.deploy());
 }
 
 void Pipeline::deploy(bool succeeded) const
 {
-    if(succeeded) {
-        log.info("Deployment successful");
-        send_email_if_enabled("Deployment completed successfully");
-    }
-    else {
+    if(!succeeded) {
         log.error("Deployment failed");
         send_email_if_enabled("Deployment failed");
+        return;
     }
+
+    log.info("Deployment successful");
+    send_email_if_enabled("Deployment completed successfully");
 }
 
 void Pipeline::send_email_if_enabled(const std::string &msg) const
 {
-    if(config.sendEmailSummary()) {
-        log.info("Sending email");
-        emailer.send(msg);
-    }
-    else {
+    if(!config.sendEmailSummary()) {
         log.info("Email disabled");
+        return;
     }
+
+    log.info("Sending email");
+    emailer.send(msg);
 }
