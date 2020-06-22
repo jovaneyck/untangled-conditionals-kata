2d7d90ee116e921b66b7524d8bfdd0c0feb42138
Van Eyck Jo <jo.vaneyck@fluxys.com>
2020-06-22 09:03:40 +0200

    add branch without tests if you want to write some yourself

---
+++ csharp/Tests/Add_Tests_here.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class Add_Tests_Here
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            Assert.Equal(6,2+4);
+        }
+    }
+}
